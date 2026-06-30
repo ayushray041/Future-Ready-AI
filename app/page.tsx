a88097ffbc4225d9a36da8fccdfa40d69b115101
Vansh Vats <vanshvats@Mac.lan>
2026-06-30 13:12:17 +0530

    Login in feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-import { ResponsiveLayout } from '@/components/responsive-layout'
-import { WelcomeHero } from '@/components/welcome-hero'
-import { DashboardCards } from '@/components/dashboard-cards'
-import { CareerRoadmap } from '@/components/career-roadmap'
-import { OpportunityEngine } from '@/components/opportunity-engine'
-import { AnalyticsSection } from '@/components/analytics-section'
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <ResponsiveLayout>
-      {/* Welcome Hero */}
-      <WelcomeHero />
-
-      {/* Dashboard Cards Grid */}
-      <DashboardCards />
-
-      {/* Career Roadmap */}
-      <CareerRoadmap />
-
-      {/* Opportunity Engine */}
-      <OpportunityEngine />
-
-      {/* Analytics & Growth */}
-      <AnalyticsSection />
-    </ResponsiveLayout>
-  )
+  redirect("/login");
 }
